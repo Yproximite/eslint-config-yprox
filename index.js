462a9f6447dfd159b989f6fcd4fc49c141d8711b
Hugo Alliaume <kocal@live.fr>
2018-12-04 21:39:01 +0100

    fix: don't configure Vue rules (for HTML in `<template>`) to prevent conflicts with Prettier (#3)
    * fix: don't configure Vue rules because of Prettier
    
    * chore: add html element with a lot of attrs

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ module.exports = {
     'no-param-reassign': ['error', { 'props': false }],
     'func-names': 'off',
     // Import
-    'import/prefer-default-export': 'off',
-    // Vue
-    'vue/max-attributes-per-line': [2, { 'singleline': 3, 'multiline': { 'max': 1, 'allowFirstLine': false } }],
-    'vue/html-closing-bracket-newline': ['error', { 'multiline': 'always' }],
+    'import/prefer-default-export': 'off'
   },
 };
